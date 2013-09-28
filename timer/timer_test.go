c3ee5ed1c6c77207c174af6eabd3e209f4c182b9
Keith Rarick <kr@xph.us>
2013-09-28 03:23:00 -0700

    add test calling print more than once

--- timer/timer_test.go
+++ timer/timer_test.go
@@ -8,6 +8,12 @@ func TestPrint(t *testing.T) {
 	defer Start().Print("arg")
 }
 
+func TestPrintMulti(t *testing.T) {
+	tm := Start()
+	tm.Print("1")
+	tm.Print("2")
+}
+
 func TestPrintf(t *testing.T) {
 	defer Start().Printf("%.2f", .9999)
 }
